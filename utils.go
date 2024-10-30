557b9040be092f4c20f6af043970f81b900339c4
NickOvt <nikolai@zone.ee>
2024-10-30 22:15:41 +0200

    add logging, flags, refactor (#5)
    Co-authored-by: NickOvt <pukpikok1@gmail.com>

--- utils.go
+++ utils.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"log"
 	"strings"
 
 	"github.com/mitchellh/mapstructure"
@@ -65,4 +66,44 @@ func sha256encode(val []byte) string {
 func getPathParams(httpString string) []string {
 	pathParams := strings.Split(strings.Split(httpString, " ")[1], "/")
 	return pathParams[1:] // first elem is empty string
+}
+
+func connectToInitialNodes() {
+	// Connect to initial nodes and get the nodes they have
+	i := 0
+	mux.Lock()
+	defer mux.Unlock()
+	// connect to maxConnections - 1 connections (so that there is always one more connection to add from outside)
+	for i < maxConnections-1 && i < len(initialNodesAdresses) {
+		mainNodeAddr := initialNodesAdresses[i]
+
+		i++
+
+		if mainNodeAddr == serverAddress {
+			continue
+		}
+
+		_, ok := existingNodesAddresses[mainNodeAddr]
+
+		if ok {
+			// already have connection for this main node
+			continue
+		}
+
+		client := connectToClient(mainNodeAddr)
+
+		if client == nil {
+			continue
+		}
+
+		// Request neighbors from other nodes
+		existingClientsAddresses[client.remoteAddr] = client
+
+		log.Println(client.conn.LocalAddr().String(), client.conn.RemoteAddr().String())
+
+		req := HTTPRequest{requestType: REQ_GET, path: "/addr", version: VERSION1_1}
+		req.setHeader("X-Own-IP", serverAddress)
+
+		client.conn.Write(req.buildBytes()) // nodes as JSON string {"nodes": []}
+	}
 }
